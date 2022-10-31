5ad21fe2e6c5e68ddf00d99260e7a6580426bf4e
Dowluri Satya Ashok <116136036+maverick0025@users.noreply.github.com>
2022-10-31 07:34:42 +0530

    Add files via upload

---
+++ arduino_based_digital_thermometer_final_coding.ino
@@ -0,0 +1,39 @@
+
+                   //ARDUINO BASED DIGITAL THERMOMETER
+
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+char f,c;
+long int lm=A0;
+float value;
+void setup() {
+  lcd.begin(16,2);
+  Serial.begin(9600);
+    lcd.print("Hello Rig");
+    lcd.setCursor(0,1);
+    lcd.print("Good Evening");
+    delay(2000);}
+
+void loop() {
+  value=analogRead(lm);
+  float cent=(value*0.488281250);
+    float farh=((1.8*cent)+32);
+lcd.clear();
+
+lcd.print("Room temp is:");
+lcd.setCursor(1,1 );
+lcd.print(cent);
+lcd.setCursor(6,1);
+lcd.print("c");
+lcd.setCursor(10,1);
+lcd.print(farh);
+lcd.setCursor(15,1);
+lcd.print("f");
+Serial.print(cent);
+Serial.print(farh);
+delay(1000);
+lcd.clear();
+lcd.home();
+delay(1000);
+}
